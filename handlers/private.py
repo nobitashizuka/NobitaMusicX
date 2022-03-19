e258600c40fde5c42f2bf9104258cc71cfaaec34
NobitaShizuka07 <97782579+NobitaShizuka07@users.noreply.github.com>
2022-03-19 23:44:09 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,14 +10,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/bb7e6f59b3db29b215446.jpg",
+        photo=f"https://te.legra.ph/file/d2f4189ab9cc2b93e1a26.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [ᴹᴿ᭄ＮＯＢＩＴＡ࿐](https://NobitaShizuka07)
 
 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ ᴹᴿ᭄ＮＯＢＩＴＡ࿐ 💜](https://t.me/NobitaShizuka07)
 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [✨ 𝐍𝐎𝐁𝐈𝐓𝐀 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 ❤️🎸](https://t.me/Nobita_Supports)
 𝐃𝐢𝐬𝐜𝐮𝐬𝐬 :- [✨  𝙉𝙊𝘽𝙄𝙏𝘼'𝙎 𝙒𝙊𝙍𝙇𝘿 🎧](https://t.me/Nobita_Ki_Duniya)
-𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨  𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗥𝗲𝗽𝗼 🌍](https://github.com/NobitaShizuka07/NobitaMusicX)
-𝐂𝐨𝐦𝐦𝐚𝐧𝐝 :- [✨𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗡𝗼𝘄 🚩](https://telegra.ph/%EA%9C%B1%E1%B4%8D%E1%B4%8F%E1%B4%8B%E1%B4%87%CA%80-%E1%B4%8D%E1%B4%9C%EA%9C%B1%C9%AA%E1%B4%84-%CA%99%E1%B4%8F%E1%B4%9B-%E1%B4%84%E1%B4%8F%E1%B4%8D%E1%B4%8D%E1%B4%80%C9%B4%E1%B4%85%EA%9C%B1-08-29)
+𝐂𝐨𝐦𝐦𝐚𝐧𝐝 :- [✨𝗖𝗹𝗶𝗰𝗸 ☑️ 𝗡𝗼𝘄 🚩](https://telegra.ph/COMMANDS-03-19-2)
 𝐅𝐞𝐞𝐋𝐢𝐧𝐠'𝐒 :- [✨ 𝗝𝗼𝗶𝗻 ❤️🥀](https://t.me/Nobita_Ki_Duniya)
 
 𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [MR.ＮＯＢＩＴＡ❤️](https://t.me/NobitaShizuka07)**""",
