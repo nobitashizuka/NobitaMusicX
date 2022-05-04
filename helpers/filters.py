44f02327ee03e470548f6a9c90cab0aa2607f446
nobitashizuka <97777683+nobitashizuka@users.noreply.github.com>
2022-05-04 08:58:42 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,12 +1,12 @@
-from typing import Union, List
+1.from typing import Union, List
 
-from pyrogram import filters
+2.from pyrogram import filters
 
-from config import COMMAND_PREFIXES
+3.from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+4.other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+5.
 
-
-def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
+6.def command(commands: Union[str, List[str]]):
+7.    return filters.command(commands, COMMAND_PREFIXES)
